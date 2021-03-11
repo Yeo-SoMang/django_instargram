8b1fd30c8bbb5c5c00e22cea197a9cf0d32d6bb0
Yeo-SoMang <sksmstkxkd159@naver.com>
2021-03-11 18:51:56 +0900

    Commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import UserCreateView, UserLoginView, UserLogoutView
+from .views import UserCreateView, UserLoginView, UserLogoutView, ContentCreateView, RelationCreateView, \
+    RelationDeleteView, UserGetView
 
 urlpatterns = [
-    path('v1/users/create/', UserCreateView.as_view(), name='apis_v1_user_create'),
-    path('v1/users/login/', UserLoginView.as_view(), name='apis_v1_user_login'),
-    path('v1/users/logout/', UserLogoutView.as_view(), name='apis_v1_user_logout'),
+    path('v1/users/create/', UserCreateView.as_view(), name='api_v1_user_create'),
+    path('v1/users/login/', UserLoginView.as_view(), name='api_v1_user_login'),
+    path('v1/users/logout/', UserLogoutView.as_view(), name='api_v1_user_logout'),
+    path('v1/users/get/', UserGetView.as_view(), name='api_v1_user_get'),
+
+    path('v1/content/create/', ContentCreateView.as_view(), name='api_v1_content_create'),
+
+    path('v1/relations/create/', RelationCreateView.as_view(), name='api_v1_relation_create'),
+    path('v1/relations/delete/', RelationDeleteView.as_view(), name='api_v1_relation_delete'),
 ]
